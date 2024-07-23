8d75f84a7665bdb576a28a4f1b52e05d2903fec2
Setiyono Ressly <setiyono.ressly@gmail.com>
2024-07-23 12:58:53 +0700

    add migration table notes

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
+require('dotenv').config();
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
     routes: {
       cors: {
         origin: ['*'],
