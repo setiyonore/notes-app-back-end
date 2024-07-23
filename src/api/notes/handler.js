1e512673c6b1a766595b37697a3f8c402d632de2
Setiyono Ressly <setiyono.ressly@gmail.com>
2024-07-23 14:06:43 +0700

    Refactor notes service to use Postgres, update handler methods to async, and add utility function for DB mapping

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -12,7 +12,7 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { title = 'untitled', body, tags } = request.payload;
 
@@ -29,7 +29,7 @@ class NotesHandler {
     return response;
   }
 
-  getNotesHandler() {
+  async getNotesHandler() {
     const notes = this._service.getNotes();
     return {
       status: 'success',
@@ -39,7 +39,7 @@ class NotesHandler {
     };
   }
 
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
       const note = this._service.getNoteById(id);
@@ -70,7 +70,7 @@ class NotesHandler {
     }
   }
 
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
@@ -102,7 +102,7 @@ class NotesHandler {
     }
   }
 
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
       this._service.deleteNoteById(id);
