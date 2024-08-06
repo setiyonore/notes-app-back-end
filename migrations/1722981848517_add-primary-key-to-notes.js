c013ff4f0c242706a4a6be21b6e53637fe26ca39
Setiyono Ressly <setiyono.ressly@gmail.com>
2024-08-07 05:08:30 +0700

    Add primary key to notes table and create collaborations table with foreign key constraints

---
+++ migrations/1722981848517_add-primary-key-to-notes.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  // Add primary key constraint to notes table
+  pgm.addConstraint('notes', 'notes_pkey', {
+    primaryKey: 'id',
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  // Remove primary key constraint from notes table
+  pgm.dropConstraint('notes', 'notes_pkey');
+};
