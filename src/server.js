09e681269256810d5f640aa30f28facfef12d4f9
Setiyono Ressly <setiyono.ressly@gmail.com>
2024-08-25 12:40:54 +0700

    feat: Improved file upload handling with S3 storage support

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
-const path = require('path');
+// const path = require('path');
 const Inert = require('@hapi/inert');
 // notes
 const notes = require('./api/notes');
@@ -27,7 +27,8 @@ const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 // uploads
 const uploads = require('./api/uploads');
-const StorageService = require('./services/storage/StorageService');
+// const StorageService = require('./services/storage/StorageService');
+const StorageService = require('./services/S3/StorageService');
 const UploadsValidator = require('./validator/uploads');
 
 const init = async () => {
@@ -35,7 +36,8 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
+  // const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
+  const storageService = new StorageService();
   const server = Hapi.server({
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'localhost',
--- src/services/S3/StorageService.js
+++ src/services/S3/StorageService.js
@@ -1,9 +1,11 @@
 const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
-const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const {
+  getSignedUrl,
+} = require('@aws-sdk/s3-request-presigner');
 
 class StorageService {
   constructor() {
-    this._s3 = new S3Client({
+    this._S3 = new S3Client({
       region: process.env.AWS_REGION,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -13,32 +15,24 @@ class StorageService {
   }
 
   async writeFile(file, meta) {
-    try {
-      const parameter = new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: meta.filename,
-        Body: file._data,
-        ContentType: meta.headers['content-type'],
-      });
-      const data = await this._s3.send(parameter);
-      console.log(data); // Check if the file is uploaded successfully
-      const url = this.createPreSignedUrl({
-        bucket: process.env.AWS_BUCKET_NAME,
-        Key: meta.filename,
-      });
-      return url;
-    } catch (error) {
-      console.error(error); // Catch and log any errors
-      return null;
-    }
+    const parameter = new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: meta.filename,
+      Body: file._data,
+      ContentType: meta.headers['content-type'],
+    });
+
+    await this._S3.send(parameter);
+
+    return this.createPreSignedUrl({
+      bucket: process.env.AWS_BUCKET_NAME,
+      key: meta.filename,
+    });
   }
 
   createPreSignedUrl({ bucket, key }) {
-    const command = new GetObjectCommand({
-      Bucket: bucket,
-      Key: key,
-    });
-    return getSignedUrl(this._s3, command, { expiresIn: 3600 });
+    const command = new GetObjectCommand({ Bucket: bucket, Key: key });
+    return getSignedUrl(this._S3, command, { expiresIn: 3600 });
   }
 }
 
